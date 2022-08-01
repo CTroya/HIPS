15b3ebbc3cb68891f4a8f853fb2c5a85eb5f9a58
Alain Vega <alainjosevz@gmail.com>
2022-08-01 01:58:36 -0400

    quick fix

--- hips/funciones/checkear_intento_acceso.sh
+++ hips/funciones/checkear_intento_acceso.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cambiar el /var/log/auth.log por /var/log/secure
-grep "Failed password for" /var/log/auth.log | grep -v grep > /var/log/hips/intento_acceso.log
+grep "Failed password for" /var/log/secure | grep -v grep > /var/log/hips/intento_acceso.log
